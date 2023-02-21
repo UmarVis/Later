a5aca0636f0b3ae733c45eb577fabf6795eca2f4
UmarVis <umar_9292@mail.ru>
2023-02-21 23:24:46 +0300

    Создал сущности для маппинга

--- src/main/java/ru/yandex/later/user/User.java
+++ src/main/java/ru/yandex/later/user/User.java
@@ -1,10 +1,29 @@
 package ru.yandex.later.user;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "users", schema = "public")
+@Getter
+@Setter
+@ToString
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "firs_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
     private String email;
-    private String name;
+    @Column(name = "registration_date")
+    private Instant registrationDate = Instant.now();
+    @Enumerated(EnumType.STRING)
+    private UserState userState;
+
 }
